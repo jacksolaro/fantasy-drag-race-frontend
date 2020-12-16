02e4156220e6f85e36c99909cf32f08f1490a2fb
jacksolaro <jacksolaro@gmail.com>
2020-12-15 21:38:13 -0800

    added logout button to nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import MenuIcon from "@material-ui/icons/Menu";
 import Drawer from "@material-ui/core/SwipeableDrawer";
+import { useHistory } from "react-router-dom";
 import "./Nav.css";
 
 import { useAuth } from "../../contexts/AuthContext";
 
 function Nav() {
-  const { currentUser } = useAuth();
+  const { currentUser, logout } = useAuth();
+  const [error, setError] = useState("");
+  const history = useHistory();
 
+  // Open / Close Drawer for Mobile Nav
   const [isExpanded, setIsExpanded] = useState(false);
   const toggleDrawer = (anchor, open) => (event) => {
     if (typeof event === "undefined") {
@@ -23,6 +27,18 @@ function Nav() {
 
     setIsExpanded(!isExpanded);
   };
+
+  // Handle Log Out
+  async function handleLogout() {
+    setError("");
+    try {
+      await logout();
+      history.pushState("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
+
   return (
     <nav className="">
       <div className={`nav`}>
@@ -55,7 +71,13 @@ function Nav() {
               </Link>
             </li>
           )}
-
+          {currentUser && (
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={handleLogout}>
+                Log Out
+              </Link>
+            </li>
+          )}
           <li className="nav-item">
             <Link className="nav-link" to="/leagues">
               Leagues
