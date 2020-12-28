afe1ff8d7ac86eaa7f15235275c4fbe7597c74b1
jacksolaro <jacksolaro@gmail.com>
2020-12-28 09:03:48 -0800

    added styling to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,64 @@
+import { Box, Container, Grid, Link, Typography } from "@material-ui/core";
+import { Copyright } from "@material-ui/icons";
 import React from "react";
 import "./Footer.css";
 
+const footers = [
+  {
+    title: "Company",
+    description: ["Team", "History", "Contact us", "Locations"],
+  },
+  {
+    title: "Features",
+    description: [
+      "Cool stuff",
+      "Random feature",
+      "Team feature",
+      "Developer stuff",
+      "Another one",
+    ],
+  },
+  {
+    title: "Resources",
+    description: [
+      "Resource",
+      "Resource name",
+      "Another resource",
+      "Final resource",
+    ],
+  },
+  {
+    title: "Legal",
+    description: ["Privacy policy", "Terms of use"],
+  },
+];
+
 function Footer() {
   return (
-    // <!-- Footer -->
     <footer className="">
-      <div className="footer-accent"></div>
-      <div className="">
-        <div className="">
-          <div className="">Copyright&copy; 2020 Jack Solaro</div>
-        </div>
-      </div>
+      <Container maxWidth="md" component="footer">
+        <Grid container spacing={4} justify="space-evenly">
+          {footers.map((footer) => (
+            <Grid item xs={6} sm={3} key={footer.title}>
+              <Typography variant="h6" color="textPrimary" gutterBottom>
+                {footer.title}
+              </Typography>
+              <ul className="footer__ul">
+                {footer.description.map((item) => (
+                  <li key={item} className="footer__li">
+                    <Link href="#" variant="subtitle1" color="textSecondary">
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
+          ))}
+        </Grid>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
     </footer>
   );
 }
