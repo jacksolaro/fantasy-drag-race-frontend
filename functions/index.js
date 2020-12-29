4f30159f1a1772c067cd01b3e867cc97b9c4aa2c
jacksolaro <jacksolaro@gmail.com>
2020-12-29 11:31:08 -0800

    added dynamic functionality around correct answers for seasons

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,31 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const { db } = require("../src/firebase");
+admin.initializeApp();
+// const { db } = require("../src/firebase.js");
+
+const db = admin.firestore();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-exports.updateScores = functions.database
-  .ref("/shows/RPDR/seasons/US_Reg_13/episodes")
-  .onWrite((event) => {
-    db.collection("leagues");
-    return "this function ran!";
-  });
+exports.calculateScores = functions.https.onRequest((request, response) => {
+  db.ref("leagues/zHl5wrliJjxuc3NegSh3")
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        response.send("Did Something");
+      } else {
+        throw new Error("Does not exist");
+      }
+      return null;
+    })
+    .catch((error) => response.send(error));
+});
+
+// exports.updateScores = functions.database
+//   .ref("/shows/RPDR/seasons/US_Reg_13/episodes")
+//   .onWrite((event) => {
+//     db.collection("leagues");
+//     return "this function ran!";
+//   });
