ec2ebcac74dfcd29fe07e700b37669e515a83324
jacksolaro <jacksolaro@gmail.com>
2021-01-05 19:44:05 -0800

    added some formatting to home page instructions

--- src/components/HomeInstructions/HomeInstructions.js
+++ src/components/HomeInstructions/HomeInstructions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import "./HomeInstructions.css";
 
 function HomeInstructions() {
@@ -12,27 +12,45 @@ function HomeInstructions() {
         It's simple, really! All you have to do is Create, Choose, and Compete!
       </Typography>
       <div>
-        <Typography gutterBottom variant="h4" component="h2" align="left">
-          1. CREATE
-        </Typography>
-        <Typography gutterBottom variant="h6" component="h6" align="left">
-          Start off by creating or joining a league. This creates a space within
-          a TV show for you and your friends to keep track of your points!
-        </Typography>
-        <Typography gutterBottom variant="h4" component="h2" align="left">
-          2. CHOOSE
-        </Typography>
-        <Typography gutterBottom variant="h6" component="h6" align="left">
-          To earn points, you will select rosters of your favorite reality tv
-          show contestants. Each week, you will earn points based on how your
-          selected contestants perform!
-        </Typography>
-        <Typography gutterBottom variant="h4" component="h2" align="left">
-          3. COMPETE
-        </Typography>
-        <Typography gutterBottom variant="h6" component="h6" align="left">
-          And that's it! You're ready to compete against your friends!
-        </Typography>
+        <Grid container spacing={0} justify="center" alignItems="center">
+          <Grid item xs={4} align="left">
+            <Typography gutterBottom variant="h4" component="h2" align="left">
+              1. CREATE
+            </Typography>
+            <Typography gutterBottom variant="h6" component="h6" align="left">
+              Start off by creating or joining a league. This creates a space
+              within a TV show for you and your friends to keep track of your
+              points!
+            </Typography>
+          </Grid>
+          <Grid item xs={8} align="center">
+            <img src={"http://placekitten.com/401/300"}></img>
+          </Grid>
+          <Grid item xs={8} align="center">
+            <img src={"http://placekitten.com/405/300"}></img>
+          </Grid>
+          <Grid item xs={4} align="right">
+            <Typography gutterBottom variant="h4" component="h2" align="left">
+              2. CHOOSE
+            </Typography>
+            <Typography gutterBottom variant="h6" component="h6" align="left">
+              To earn points, you will select rosters of your favorite reality
+              tv show contestants. Each week, you will earn points based on how
+              your selected contestants perform!
+            </Typography>
+          </Grid>
+          <Grid item xs={4} align="right">
+            <Typography gutterBottom variant="h4" component="h2" align="left">
+              3. COMPETE
+            </Typography>
+            <Typography gutterBottom variant="h6" component="h6" align="left">
+              And that's it! You're ready to compete against your friends!
+            </Typography>
+          </Grid>
+          <Grid item xs={8} align="center">
+            <img src={"http://placekitten.com/400/300"}></img>
+          </Grid>
+        </Grid>
       </div>
     </section>
   );
